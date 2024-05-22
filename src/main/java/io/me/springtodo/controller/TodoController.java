141a50d3e634905fe75c964ddfad52cd216cec18
Imnotcoderdude <topsolo7@naver.com>
2024-05-22 19:04:57 +0900

    🆙 update : 단건 조회 기능 추가

--- src/main/java/io/me/springtodo/controller/TodoController.java
+++ src/main/java/io/me/springtodo/controller/TodoController.java
@@ -3,24 +3,32 @@ package io.me.springtodo.controller;
 import io.me.springtodo.dto.TodoRequestDto;
 import io.me.springtodo.dto.TodoResponseDto;
 import io.me.springtodo.repository.Todo;
+import io.me.springtodo.repository.TodoRepository;
 import io.me.springtodo.service.Todoservice;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api")
 public class TodoController {
     public final Todoservice todoservice;
+    private final TodoRepository todoRepository;
+
     // 아무것도 없는 상태이더라도 postman 에서 호출을 하게 된다면 200 코드가 나오면서 정상호출이 되었음을 알린다.
-    @PostMapping("/v1.0/todo")
+    @PostMapping("/todo")
     public ResponseEntity<TodoResponseDto> postTodo(@RequestBody TodoRequestDto dto) {
 
         Todo todo = todoservice.createTodo(dto);
         TodoResponseDto response = new TodoResponseDto(todo);
         return ResponseEntity.ok().body(response);
-
+    }
+    // 경로변수{todoId}로 todoId 받아오기.
+    @GetMapping("/todo/{todoId}")
+    public ResponseEntity<TodoResponseDto> getTodo(@PathVariable Long todoId) {
+        Todo todo = todoservice.getTodo(todoId);
+        TodoResponseDto response = new TodoResponseDto(todo);
+        return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/io/me/springtodo/dto/TodoResponseDto.java
+++ src/main/java/io/me/springtodo/dto/TodoResponseDto.java
@@ -9,13 +9,15 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class TodoResponseDto {
+    private Long todoId;
     private String title;
     private String content;
     private String userName;
-    // Builder 패턴
     private LocalDateTime createdAt;
 
+    // Builder 패턴
     public TodoResponseDto(Todo todo) {
+        this.todoId = todo.getTodoId();
         this.title = todo.getTitle();
         this.content = todo.getContent();
         this.userName = todo.getUserName();
